a86520d83d66af77a3ed41249586414a2148b53b
Alex Leigh <leigh@alexleigh.me>
2012-08-14 17:19:34 -0400

    init_scopes complete.

--- oauth2/management/commands/init_scopes.py
+++ oauth2/management/commands/init_scopes.py
@@ -1,7 +1,9 @@
 import logging
 
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+
+from ...models import Scope
+from ...settings import SCOPES
 
 log = logging.getLogger(__name__)
 
@@ -10,7 +12,8 @@ class Command(BaseCommand):
            'OAUTH2_SCOPES variable in settings.'
 
     def handle(self, *args, **options):
-        users = User.objects.all()
+        Scope.objects.all().delete()
         
-        for user in users:
-            pass
+        for name, description in SCOPES:
+            scope = Scope(name=name, description=description)
+            scope.save()
