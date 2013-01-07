f2efc8a14d77c498e6063d1a3f2df9e474338f7b
Alex Leigh <leigh@alexleigh.me>
2013-01-06 19:08:49 -0800

    Updated names

--- README.rst
+++ README.rst
@@ -1,3 +1,3 @@
-This is an OAuth2 provider for Django, based on github.com/hiidef/oauth2app.
+This is an OAuth2 server for Django, based on github.com/hiidef/oauth2app.
 Unfortunately, due to significant modifications to the original project, the
 documentation is no longer up-to-date.
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "django-oauth2",
-    version = "0.4",
+    name = "django-oauth2server",
+    version = "0.4.1",
     author = "Alex Leigh",
     author_email = "leigh@alexleigh.me",
     description = "Django OAuth 2.0 Server",
     license = "MIT License",
-    keywords = "django oauth2",
-    url = "https://github.com/atomatica/django-oauth2",
+    keywords = "django oauth2 server",
+    url = "https://github.com/alexleigh/django-oauth2server",
     include_package_data = True,
     packages = find_packages()
 )
